754d330c64e9874ae94b78e6285532df9332e8ae
Filipe Magalhães Moreira <filipe.mm.1999@hotmail.com>
2022-11-30 16:54:17 -0300

    delete da public/criação do pedido

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ClientesController = require("../controllers/clientesController")
 const ProdutosController = require("../controllers/produtosController")
+const PedidosController = require("../controllers/pedidosController")
 
 //rotas clientes
 router.get('/clientes', ClientesController.index);
@@ -16,6 +17,13 @@ router.post('/produtos', ProdutosController.create);
 router.get('/produtos/:id', ProdutosController.show);
 router.delete('/produtos/:id', ProdutosController.delete);
 router.put('/produtos/:id', ProdutosController.update);
+
+//rotas pedidos
+router.get('/pedidos', PedidosController.index);
+router.post('/pedidos', PedidosController.create);
+router.get('/pedidos/:id', PedidosController.show);
+router.delete('/pedidos/:id', PedidosController.delete);
+router.put('/pedidos/:id', PedidosController.update);
 
 
 
