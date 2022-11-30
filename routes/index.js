cde9f0d22043fee649c257507c055f12254826d4
SungJu <61665463+Sungjuno@users.noreply.github.com>
2022-11-30 18:18:52 -0300

    atualizando index sung da main

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const HomeController = require("../controllers/homeController")
+
+//imports dos Controllers
 const ClientesController = require("../controllers/clientesController")
+const ProdutosController = require("../controllers/produtosController")
+const PedidosController = require("../controllers/pedidosController")
+const PedidosProdutosController = require("../controllers/pedidosProdutosController")
 
 //rotas clientes
 router.get('/clientes', ClientesController.index);
@@ -9,6 +13,27 @@ router.post('/clientes', ClientesController.create);
 router.get('/clientes/:id', ClientesController.show);
 router.delete('/clientes/:id', ClientesController.delete);
 router.put('/clientes/:id', ClientesController.update);
+
+//rotas produtos
+router.get('/produtos', ProdutosController.index);
+router.post('/produtos', ProdutosController.create);
+router.get('/produtos/:id', ProdutosController.show);
+router.delete('/produtos/:id', ProdutosController.delete);
+router.put('/produtos/:id', ProdutosController.update);
+
+//rotas pedidos
+router.get('/pedidos', PedidosController.index);
+router.post('/pedidos', PedidosController.create);
+router.get('/pedidos/:id', PedidosController.show);
+router.delete('/pedidos/:id', PedidosController.delete);
+router.put('/pedidos/:id', PedidosController.update);
+
+//rotas pedidosProdutos
+router.get('/pedidosProdutos', PedidosProdutosController.index);
+router.post('/pedidosProdutos', PedidosProdutosController.create);
+router.get('/pedidosProdutos/:id', PedidosProdutosController.show);
+router.delete('/pedidosProdutos/:id', PedidosProdutosController.delete);
+router.put('/pedidosProdutos/:id', PedidosProdutosController.update);
 
 
 module.exports = router;
